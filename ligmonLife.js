c4340cc3543edf5b26cc59358496e2502549c5a8
ligmonLife.js <biancaligmon93@gmail.com>
2025-09-10 09:45:23 +0800

    Added condition for study hours

--- ligmonLife.js
+++ ligmonLife.js
@@ -1,38 +1,35 @@
 console.log("IT21C");
 
-let student="Bianca Ligmon";
-let course="Bachelor of Science in Information Studies";
-let yearlevel="2nd Year";
+let student = "Bianca Ligmon";
+let course = "Bachelor of Science in Information Studies";
+let yearlevel = "2nd Year";
 
-console.log("Student Name: "+ student + "\nCourse: + course + "\nYear Level:"+ yearlevel +"\n");
+console.log("Student Name: " + student + 
+            "\nCourse: " + course + 
+            "\nYear Level: " + yearlevel + "\n");
 
-let hoursOfSleep= 8;
-let hoursOfStudy= 2;
+let hoursOfSleep = 8;
+let hoursOfStudy = 2;
 
 let totalHours = hoursOfSleep + hoursOfStudy;
 
 console.log("Hours of Sleep: " + hoursOfSleep + " hours");
-console.log("Hours of Study: " + hoursOfStudy +  "hours");
-console.log("Total Hours of Sleep and Study:" + totalHours +" hours");
+console.log("Hours of Study: " + hoursOfStudy + " hours");
+console.log("Total Hours of Sleep and Study: " + totalHours + " hours");
 
-if (study>5) {
-console.log("Great! You studied enough today.");
-
+if (hoursOfStudy > 5) {
+    console.log("Great! You studied enough today.");
 } else {
-console.log("You need to study more.");
+    console.log("You need to study more.");
 }
 
-let subjects=["Web System", "Event-Driven", "00P"];
-console.log("List of my subjects this sem:");
-for (let 1=0: 1 <subjects.length; i++) {
-
-Console.log(subjects [1]);
+let subjects = ["Web Systems", "Event-Driven Programming", "OOP"];
+console.log("\nList of my subjects this sem:");
+for (let i = 0; i < subjects.length; i++) {
+    console.log(subjects[i]);
 }
+
 function studentSummary() {
-
-console.log("My name is "+name+", a "+year+" from "+course);
-return studentSummary;
-
+    console.log("\nMy name is " + student + ", a " + yearlevel + " student from " + course + ".");
 }
-
-console.log(studentSummary());
+studentSummary();
